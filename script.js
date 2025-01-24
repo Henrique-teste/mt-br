137f05130d99e2bdcb30719fd71892f5b0894d74
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2025-01-24 12:56:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,16 @@ btn.addEventListener('click', function (e) {
 function validaNome() {
   let nome = document.getElementById('nome').value;
   if (nome.length >= 5) {
-    nome.style.color = 'green';
     alert('valido')
   } else {
-    nome.style.color = 'red';
     alert('invalido')
   }
 }
 function validaSenha() {
   let senha = document.getElementById('senha').value;
   if (senha.length >= 5) {
-    senha.style.background = 'green';
     alert('valido')
   } else {
-    senha.style.background = 'red';
     alert('invalido')
   }
 }
