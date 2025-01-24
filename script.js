43e37663216f23ed9ba1dd1c912b90e561a94070
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2025-01-24 13:17:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function validaNome() {
   let nome = document.getElementById('nome').value;
   if (nome.length >= 5) {
     alert('Seu nome é valido')
-    nome.style.display = 'block'
   } else {
     alert('invalido')
   }
@@ -15,7 +14,6 @@ function validaSenha() {
   let senha = document.getElementById('senha').value;
   if (senha.length >= 5) {
         alert('Sua senha é valido')
-    btn.style.display = 'block'
   } else {
     alert('invalido')
   }
