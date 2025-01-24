8e23efd6d963328c448fbb3ce6031e451fb43ade
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2025-01-24 11:28:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+  validaNome();
+});
+function validaNome() {
+  let nome = document.getElementById('nome').value;
+  if (nome.length > 5) {
+    alert('valido');
+  } else {
+    alert('invalido');
+  }
+}
