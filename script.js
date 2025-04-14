22a54122c096378024a5be7eb2223dca0f6bb273
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2025-04-14 11:27:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let nome = document.getElementById('nome').value
 let btn1 = document.getElementById('btn1')
 let btn2 = document.getElementById('btn2')
+
+btn1.addEventListener('click', function(e){
+    e.preventDefault
+    console.log(nome.value)
+})
