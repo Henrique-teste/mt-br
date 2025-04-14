eadb5f84cd9a0036e5bb043ec8deda37097b63ce
Henrique-teste <152813884+Henrique-teste@users.noreply.github.com>
2025-04-14 10:54:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let nome = document.getElementById('nome').value
 
-btn1.addEventListerner('submit',function(){
+btn1.addEventListener('submit',function(){
 e.preventDefault();
 alert(nome.value)
 })
